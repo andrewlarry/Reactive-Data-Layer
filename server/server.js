73412024a7a2e634aeb2b40153134f778e185a9d
Andrew <afuselier23@gmail.com>
2018-03-10 16:03:27 -0800

    Working commit

--- server/server.js
+++ server/server.js
@@ -16,11 +16,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(express.static(path.join(__dirname, '../client/public')));
-app.use('/graphql', graphqlHTTP({ schema, context: {user: 'Andrew'}, graphiql: true, extensions: (test) => console.log('test') }));
+app.use('/graphql', graphqlHTTP({ schema, context: {user: 'Andrew'}, graphiql: true, extensions: (test) => sendUpdates }));
 
 app.get('/', (req, res) => res.send('hi'));
 
-app.listen(3000);
-console.log('listening on 3000');
+app.listen(3001);
+console.log('listening on 3001');
+
+const sendUpdates = (val) => {
+  // io.sockets.emit(handle, 'whatever the fuck I want');
+  // console.log('Started from the bottom. Now were here. --Drake');
+  console.log(val);
+  //io.sockets.emit('news', val);
+  const handlers = Object.keys(RDL.queue);
+  handlers.forEach((handler) => {
+    const query = RDL.subscriptions[handler];
+    console.log('hello world');
+    graphql(schema, query).then((result) => {
+      console.log(result);
+      io.sockets.emit(handler, result);
+    }); // io.sockets.emit(handle, 'whatever the fuck I want'););
+  });
+  return val;
+};
 
 module.exports = app;
